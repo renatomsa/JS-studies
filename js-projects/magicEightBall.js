162cb8816c6bd19a681c4d9c6cae34368ce41659
Renato Serrano <renatom.sandrade@gmail.com>
2022-11-22 03:48:14 -0300

    Update magicEightBall.js
    adding comments

--- js-projects/magicEightBall.js
+++ js-projects/magicEightBall.js
@@ -4,7 +4,7 @@ userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 let userQuestion = "Am I going to learn JavaScript?";
 console.log(`${userName} asks: '${userQuestion}'`);
 
-let randomNumber = Math.floor(Math.random() * 8);
+let randomNumber = Math.floor(Math.random() * 8); //Math.random generates a number between 0 (inclusive) and 1 (exclusive), so we have to multiply by 8 to achieve the range we want (7) and then round down the number using Math.floor
 let eightBall = "";
 
 switch (randomNumber) {
